f24695a60a30aa6dc7a86ee4db95a74aa61bc882
carla-piazza1 <91873098+carla-piazza1@users.noreply.github.com>
2022-03-14 11:22:41 +0100

    jour05 fini

---
+++ jour05/script.js
@@ -0,0 +1,10 @@
+function afficherjourssemaines(){
+
+    let jourssemaines = ['lundi','mardi','mercredi','jeudi','vendredi','samedi','dimanche'];
+    
+    for ( x =0; x < jourssemaines.length; x++){
+        console.log (jourssemaines[x]);
+    }
+}
+
+afficherjourssemaines();
