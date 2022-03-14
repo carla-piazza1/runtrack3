42ccc9d9c19baab44d41d7a19b030f083a15ea56
carla-piazza1 <91873098+carla-piazza1@users.noreply.github.com>
2022-03-14 11:11:14 +0100

    jour04 fini

---
+++ jour04/script.js
@@ -0,0 +1,13 @@
+function bisextille(année){
+    
+    if (année % 4 == 0){
+        
+        return console.log ('true');
+    } 
+    
+    else {
+        return console.log ('false')
+    }
+}
+
+bisextille(2012);
